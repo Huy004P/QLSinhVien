6c343a463b00c02f6cbf6cd7058fe616c2d4feca
Huy004P <114895245+Huy004P@users.noreply.github.com>
2023-05-05 09:48:41 +0700

    Add files via upload

---
+++ src/advance/dev/SortStudentByGPA.java
@@ -0,0 +1,17 @@
+package advance.dev;
+
+import java.util.Comparator;
+
+/**
+ * SortStudentByGPA class
+ * 
+ */
+public class SortStudentByGPA implements Comparator<Student> {
+    @Override
+    public int compare(Student student1, Student student2) {
+        if (student1.getGpa() > student2.getGpa()) {
+            return 1;
+        }
+        return -1;
+    }
+}
---
+++ src/advance/dev/SortStudentByName.java
@@ -0,0 +1,14 @@
+package advance.dev;
+
+import java.util.Comparator;
+
+/**
+ * SortStudentByName class
+ * 
+ */
+public class SortStudentByName implements Comparator<Student> {
+    @Override
+    public int compare(Student student1, Student student2) {
+        return student1.getName().compareTo(student2.getName());
+    }
+}
